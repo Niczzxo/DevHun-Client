dbca1dda90d86eea69e41998df2f2c6762f2fd00
Nixzzxo <riyazulislam.773@gmail.com>
2026-04-21 16:17:12 +0600

    style: update GoogleButton component styles for consistency and remove unused eslint comment

--- src/components/ui/GoogleButton/GoogleButton.jsx
+++ src/components/ui/GoogleButton/GoogleButton.jsx
@@ -1,6 +1,5 @@
 import { FcGoogle } from "react-icons/fc";
 import ActionSpinner from "../ActionSpinner/ActionSpinner";
-// eslint-disable-next-line no-unused-vars
 import * as motion from "motion/react-client";
 
 const buttonVariants = {
@@ -25,7 +24,7 @@ const GoogleButton = ({ onClick, disabled }) => {
         type="button"
         disabled={disabled}
         onClick={onClick}
-        className="btn btn-sm md:btn-md btn-block bg-white text-neutral dark:bg-neutral dark:border-neutral dark:shadow-white/20 dark:hover:shadow-md transition-shadow duration-300 dark:shadow dark:text-white border-[#e5e5e5]"
+        className="btn btn-sm md:btn-md btn-block bg-white text-slate-900 hover:bg-slate-50 border-slate-200 dark:bg-slate-900 dark:text-white dark:border-slate-800 dark:hover:bg-slate-800 transition-all duration-300 shadow-sm"
       >
         {disabled ? (
           <ActionSpinner />
